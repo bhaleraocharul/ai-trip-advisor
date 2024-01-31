c8c885275d864e89a178703cb34323f712c5d8da
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-02-01 09:09:21 +1100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ function generateTrips(event) {
   let apiKey = "1o630f4a9d3497230622bb9ffta16859";
   let context =
     "You are a trip advisor who has traveled the world. each destination is a new item on an ordered list using the <ol> and <li> tags";
-  let prompt = `Give top 3 destinations about the city inserted in ${instructionsInput.value}`;
+  let prompt = `Give top 3 destinations about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let destinationElement = document.querySelector("#trip-destination");
