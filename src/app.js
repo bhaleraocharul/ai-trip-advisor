3b0b27dec9bda75acc35183b0a185d22a8cc7f79
charul <142137074+bhaleraocharul@users.noreply.github.com>
2024-01-31 21:40:46 +1100

    added js, css, html

---
+++ src/app.js
@@ -0,0 +1,28 @@
+function displayTrips(response) {
+  new Typewriter("#trip-destination", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
+function generateTrips(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-input");
+  let apiKey = "1o630f4a9d3497230622bb9ffta16859";
+  let context =
+    "You are a trip advisor who has traveled the world. Give each destination on a new line <br/>";
+  let prompt = `Give top 3 destinations about the city inserted in ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let destinationElement = document.querySelector("#trip-destination");
+  destinationElement.classList.remove("hidden");
+  destinationElement.innerHTML = `<div class="blink">⏳ Generating top 3 places in ${instructionsInput.value}</div>`;
+
+  axios.get(apiUrl).then(displayTrips);
+}
+
+let poemFormElement = document.querySelector("#trip-generator-form");
+poemFormElement.addEventListener("submit", generateTrips);
